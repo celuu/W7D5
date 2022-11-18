424b84bb6217ee48249f4ee290915c609740f4d5
Thomas Conger <t.e.conger@gmail.com>
2022-11-18 10:29:25 -0800

    added users controller

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.1"
 
+gem "bcrypt"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
 
@@ -27,4 +29,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-   
+
     validates :username, :password_digest, :session_token, presence: true
     validates :username, :session_token, uniqueness: true
     validates :password, length:{minimum: 6}, allow_nil: true
@@ -11,9 +11,9 @@ class User < ApplicationRecord
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
         if user && user.is_password?(password)
-            return user 
+            return user
         else
-            return nil 
+            return nil
         end
     end
 
@@ -34,7 +34,7 @@ class User < ApplicationRecord
     end
 
     def ensure_session_token
-        @session_token ||= generate_session_token
+        self.session_token ||= generate_session_token
     end
 
     def generate_session_token
