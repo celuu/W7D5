310e7e88b527dd62f82d2c58ca427d083ca3a990
celuu <christine.e.luu@gmail.com>
2022-11-18 10:59:19 -0800

    Added User Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+User.create(username: "tom", password: "123456")
+User.create(username: "christine", password: "christineiscool")
+User.create(username: "taylor", password: "ilovetaylorswift")
+User.create(username: "tailor", password: "pantsrcool")
+
+
+
+
